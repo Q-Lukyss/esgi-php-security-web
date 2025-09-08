e50161eb2201894407a5dee9f3339c000643d318
Q-Lukyss <quentin.lkss@gmail.com>
2025-09-08 16:27:18 +0200

    fonctionnel avec template

--- app/public/index.php
+++ app/public/index.php
@@ -7,16 +7,20 @@ use Flender\Dash\Response\Response;
 
 $app = new Router();
 
-$app->set_base_path('/lpm')
+$app->set_base_path('')
     ->set_static_path('/static', __DIR__ . '/../public')
     ->set_404_callback(fn() => new Response('Custom Not Found Page', 404))
     ->set_error_callback(fn() => new Response('Custom Error Page', 500));
 
 $app->get('/', function() {
-    return "<h1>Home Page</h1>";
+    require __DIR__ . '/../src/templates/index.php';
 });
 
-$app->set_controllers_directory(__DIR__ . '/../app/Controllers');
+$app->get('/about', function() {
+    require __DIR__ . '/../src/templates/about.html';
+});
+
+// $app->set_controllers_directory(__DIR__ . '/../app/Controllers');
 
 // $app->set_container([
 //     PDO::class => function() {
