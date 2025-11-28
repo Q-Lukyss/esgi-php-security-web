27f4bb5b884c7c405520f05d1f94ab3509001172
FlenderGit <tristan.deloeil@gmail.com>
2025-11-28 09:41:06 +0100

    ADD: Flake support

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    devShells.x86_64-linux.default = let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [
+            php83
+            php83Packages.composer
+          ];
+        };
+  };
+}
